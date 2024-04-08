38120bd7b188226f6cf629b96437a2a33cbbcd47
Riko-04 <echoge11@gmail.com>
2024-04-08 18:44:06 +0300

    Final commit

--- script.js
+++ script.js
@@ -7,19 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
     const buyTicketBtn = document.getElementById('buy-ticket');
   
     // Movie details
-    const movies = {
-        "Game of Thrones": "Nine noble families wage war against each other in order to gain control over the mythical land of Westeros. Meanwhile, a force is rising after millenniums and threatens the existence of living men.",
-        "Teen Wolf": "Scott McCall, an awkward teen, turns into a werewolf after being bitten by one a day before sophomore year. He must learn to adapt to his new identity while handling life as a teenager.",
-        "Kong Skull Island": "A crew that reaches Skull Island to map it, is attacked by a humongous ape. The survivors then regroup to find out more about the ape, the island's natives and underground monsters.",
-        "Furiosa": "Snatched from the Green Place of Many Mothers, young Furiosa falls into the hands of a great biker horde led by the warlord Dementus. Sweeping through the Wasteland, they come across the Citadel, presided over by the Immortan Joe. As the two tyrants fight for dominance, Furiosa soon finds herself in a nonstop battle to make her way home.",
-        "Fast and Furious 11": "Fast & Furious 11, also known as Fast X Part 2, is the eleventh film in the Fast & Furious series, the twelfth film overall, and the concluding film of main Fast & Furious timeline. The film will be directed by Louis Leterrier and released on April 4th, 2025.",
-        "The Boys": "Superheroes are often as popular as celebrities, as influential as politicians, and sometimes even as revered as gods. But that's when they're using their powers for good. What happens when the heroes go rogue and start abusing their powers?",
-        "House of the Dragon": "After the death of their father, two siblings fight for the throne, thereby causing a civil war known as the Dance of the Dragons.",
-        "Parasyte: The Grey": "People must rise to combat unidentified parasites after they violently take over human hosts and gain power.",
-        "Heart of the Hunter": "The quiet retirement of a once-feared assassin in South Africa is interrupted by a desperate call from the daughter of an old and trusted friend whose father has been kidnapped. He uncovers a conspiracy at the heart of the government.",
-        "The Incredibles": "Forced to adopt a civilian identity and stuck in a white-collar job, Mr Incredible itches to get back into action. When he is lured into a trap by the evil Syndrome, his family contrives to save him.",
-        "She-hulk": "Jennifer Walters has a complicated life as a single, 30-something attorney who also happens to be a green 6-foot-7-inch superpowered hulk.",
-        "Dawn of the planets of the apes": "Caesar, a chimpanzee and leader of the evolved apes, allows a human family into his territory. However, due to a vindictive bonobo, misunderstandings arise between the two species, resulting in war."
+    const movieDetails = {
+        "Game of Thrones": {
+            description: "Nine noble families wage war against each other in order to gain control over the mythical land of Westeros. Meanwhile, a force is rising after millenniums and threatens the existence of living men.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img1.jpg"
+        },
+        "Teen Wolf": {
+            description: "Scott McCall, an awkward teen, turns into a werewolf after being bitten by one a day before sophomore year. He must learn to adapt to his new identity while handling life as a teenager.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img2.jpg"
+        },
+        "Kong Skull Island": {
+            description: "A crew that reaches Skull Island to map it, is attacked by a humongous ape. The survivors then regroup to find out more about the ape, the island's natives and underground monsters.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img3.jpg"
+        },
+        "Furiosa": {
+            description: "Snatched from the Green Place of Many Mothers, young Furiosa falls into the hands of a great biker horde led by the warlord Dementus. Sweeping through the Wasteland, they come across the Citadel, presided over by the Immortan Joe. As the two tyrants fight for dominance, Furiosa soon finds herself in a nonstop battle to make her way home.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img4.jpg"
+        },
+        "Fast and Furious 11": {
+            description: "Fast & Furious 11, also known as Fast X Part 2, is the eleventh film in the Fast & Furious series, the twelfth film overall, and the concluding film of main Fast & Furious timeline. The film will be directed by Louis Leterrier and released on April 4th, 2025.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img5.webp"
+        },
+        "The Boys": {
+            description: "Superheroes are often as popular as celebrities, as influential as politicians, and sometimes even as revered as gods. But that's when they're using their powers for good. What happens when the heroes go rogue and start abusing their powers?",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img6.jpgg"
+        },
+        "House of the Dragon": {
+            description: "After the death of their father, two siblings fight for the throne, thereby causing a civil war known as the Dance of the Dragons.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img7.jpeg"
+        },
+        "Parasyte: The Grey": {
+            description: "People must rise to combat unidentified parasites after they violently take over human hosts and gain power.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img8.jpg"
+        },
+        "Heart of the Hunter": {
+            description: "The quiet retirement of a once-feared assassin in South Africa is interrupted by a desperate call from the daughter of an old and trusted friend whose father has been kidnapped. He uncovers a conspiracy at the heart of the government.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img9.jpeg"
+        },
+        "The Incredibles": {
+            description: "Forced to adopt a civilian identity and stuck in a white-collar job, Mr Incredible itches to get back into action. When he is lured into a trap by the evil Syndrome, his family contrives to save him.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img10.jpeg"
+        },
+        "She-hulk": {
+            description: "Jennifer Walters has a complicated life as a single, 30-something attorney who also happens to be a green 6-foot-7-inch superpowered hulk.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/image11.jpeg"
+        },
+        "Dawn of the planets of the apes": {
+            description: "Caesar, a chimpanzee and leader of the evolved apes, allows a human family into his territory. However, due to a vindictive bonobo, misunderstandings arise between the two species, resulting in war.",
+            imagePath: "/home/riko/home/phase1/flatdango-code-challenge/images/img12.jpeg"
+        }
     };
   
     // Event listener for clicking on a movie title
@@ -45,9 +81,10 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Function to update movie details
     function updateMovieDetails(movieTitle) {
-        poster.src = `assets/${movieTitle.replace(/\s+/g, '').toLowerCase()}.jpg`;
+        const { description, imagePath } = movieDetails[movieTitle];
+        poster.src = imagePath;
         title.textContent = movieTitle;
-        description.textContent = movies[movieTitle];
+        description.textContent = description;
         ticketNum.textContent = `${remainingTickets} remaining tickets`;
     }
   
